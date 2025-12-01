0e5cb353d9b07f5ecfa69cce14c2138beaa54c4f
ahmed-1430 <ahmedtexting@gmail.com>
2025-12-02 01:25:37 +0600

    DB connected

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+let db, Services, Bookings, Reviews;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("WarmPaws"); 
+    Services = db.collection("services");
+    Bookings = db.collection("bookings");
+    Reviews = db.collection("reviews");
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+  }
+}
+
+connectDB();
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server is Running.....')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
